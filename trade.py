8c8a213b9671f3242285dd230692748028ab9de8
Eryk <erykdomanski2808@gmail.com>
2026-07-16 19:12:47 +0200

    Implement Trade model

--- trade.py
+++ trade.py
@@ -11,28 +11,67 @@
 # date -> date
 # notes -> overall note -> I will set the limit for maximum 350 characters
 
+from datetime import datetime
 
 class Trade:
     
-    def __init__(self, pair, direction, market_condition,
-                  entry, exit, rr, 
-                  session, result,
-                  date, notes,
-                  was_valid):
+    def __init__(self, was_valid, date,
+                session, pair, direction, market_condition,
+                entry, exit, rr, 
+                result,
+                notes
+                ):
+        self.was_valid = was_valid # boolean
+        self.date = date # I will need data module
+        self.session = session # session
         self.pair = pair # string
         self.direction = direction # string
         self.market_condition = market_condition # string
         self.entry = entry #float
         self.exit = exit # float
         self.rr = rr # float
-        self.session = session # session
         self.result = result #string
-        self.date = date # I will need data module
         self.notes = notes # big string
-        self.was_valid = was_valid # boolean
 
     def __str__(self):
-        pass
+        return (f"Trade:\n"
+                f"Trade was valid?: {bool(self.was_valid)}"
+                f"\nDate: {self.date.strftime('%Y-%m-%d')}"
+                f"\nSession: {self.session}"
+                f"\nPair: {self.pair}"
+                f"\n---------------------------------------------------------"
+                f"\nDirection: {self.direction}"
+                f"\nMarket Condition: {self.market_condition}"
+                f"\n---------------------------------------------------------"
+                f"\nRR: {self.rr}"
+                f"\nResult: {self.result}"
+                f"\n---------------------------------------------------------"
+                f"\nEntry/exit info:"
+                f"\n\tEntry: {self.entry}"
+                f"\n\tExit: {self.exit}"
+                f"\n---------------------------------------------------------"
+                f"\n\t\tNotes: {self.notes}"
+            )
 
-    def to_dict(self):
-        pass
+    def to_dict(self,):
+        
+        documentation = { # Didn't added entry/exit cus its useless here tbh
+            "was_valid": self.was_valid,
+            "date": self.date,
+            "session": self.session,
+            "pair": self.pair,
+            "direction": self.direction,
+            "market_condition": self.market_condition,
+            "rr": self.rr,
+            "result": self.result,
+            "entry": self.entry,
+            "exit": self.exit,
+            "notes": self.notes 
+        }
+        return documentation
+# date - # "%Y, %m, %d"
+trade = Trade(True, datetime.strptime("2026-07-16", "%Y-%m-%d"), "London", "XAUUSD", "Long", "Trending Bullish",
+              7572.43, 7500.40, 1.99, "W",
+              "Example notes") 
+print(trade)
+print(trade.to_dict())
