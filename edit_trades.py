b7e586e49f49db2d476d3812f72b55ee8b9c36a5
Eryk <erykdomanski2808@gmail.com>
2026-08-10 17:21:27 +0200

    feat: add trade parameter workflow skeleton - 70% done

--- edit_trades.py
+++ edit_trades.py
@@ -1,42 +1,91 @@
+from journal import Journal
+from trade_statistics import Statistics
+from trade_input import guide
 
-def edit_trade():
-    pass
+journal= Journal()
+
+stats = Statistics(journal.trades)
+
+def edit_trade(journal, trades, stats):
+    if (input("Do you want to see the journal? Y/N: ")
+    .upper().strip()== "Y"
+    ):
+        journal.display_trades()
+
+    trade_id = input("Enter trade ID: ")
+    trade = stats.id_find(trade_id)
+    print(trade)
+
+    if (
+        input("BEFORE YOU CHANGE ANYTHING:"
+        "1. You can't change trades ID" \
+        "\n2. You have to follow the guidelines" \
+        "\n3. If you want to see the guidelines Y/N: ")
+        .upper().strip() == "Y"
+    ):
+        guide()
+        print("\n\n\t\tList of parameters:" \
+        "was_valid, date, session, pair, direction" \
+        "market_condition, entry, exit, rr, result, notes")
+
+
+    parameter = input("What parameter do you want to change?:").strip()
+    if parameter == "was_valid":
+        new_value = input("New value: ").upper().strip()
+        trade.was_valid = new_value
+
+    elif parameter == "date":
+        new_value = input("New value: ")
+        trade.date = new_value
+
+    elif parameter == "session":
+        new_value = input("New value: ").upper().strip()
+        trade.session = new_value
+
+    elif parameter == "pair":
+        new_value = input("New Value: ").upper().strip()
+        trade.pair = new_value
+
+    elif parameter == "direction":
+        new_value = input("New value: ").upper().strip()
+        trade.direction = new_value
+
+    elif parameter == "market_condition":
+        new_value = input("New value: ").upper().strip()
+        trade.market_condition = new_value
+
+    elif parameter == "entry":
+        new_value = float(input("New value: ").strip())
+        trade.entry = new_value
+
+    elif parameter == "exit":
+        new_value = float(input("New value: ").strip())
+        trade.exit = new_value
+
+    elif parameter == "rr":
+        new_value = float(input("New value: ").strip())
+        trade.rr = new_value
+
+    elif parameter == "result":
+        new_value = input("New value: ").upper().strip()
+        trade.result = new_value
+
+    elif parameter == "notes":
+        new_value = input("New value:")
+        trade.notes = new_value
+
+    else:
+        print("Please enter correct parameter name")
+
+
+# show the change
+# confirm
+# if Y
+# change and save
 
 
 
 #edit trade skeleton
-# firstly import necessary modules.
-
-# for trade in self.trades
-# input trade ID
-# use if_find function and get t his trade
-# display trade
-
-# do that to every parameter possible (WITHOUT ID)
-# But make ID visible to choose
-# when user will choose ID make a print about 
-# not being able to change ID because its automatical so the user
-# will have more clarity about the program
-
-# Askwhat parameter user wants to change
-
-# USER MUST follow the guidelines ( You cant import guidelines module)
-# and just show if hes not familiar - make it optionable ofc
-# example
-# result = only valid
-# session only valid ones
-# notes -> text
-# date -> date with ..0..0.... etc
-# was valid Y/N etc etc.
-
-#Check which parameter was selected
-
-# if user_input == "result":
-#   edit result
-
-
-# Ask for a new value
-#   validation 
 
 #   show change & ask for confirmation
 
