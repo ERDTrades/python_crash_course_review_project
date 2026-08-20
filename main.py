523757faf848ea84e822fa9b36470891d28683dd
Eryk <erykdomanski2808@gmail.com>
2026-08-20 17:35:52 +0200

    Update TODO

--- main.py
+++ main.py
@@ -10,19 +10,38 @@ if __name__ == "__main__":
 
 # TODO
 
-# Edit trade
+# Make entry and exit :.2f only while showing display_trades
+
+# Polish the display_trade dataframe - make it clearer, nicer
+
+# Add a meaningfull hover to the plotly + 
+# matplotlib overall statistics (visualization)
 
 
+#Let the user decide which visualization to display:
+# plotly
+# matplotlib
+# both
 
-# Charts
-# 2 opions matplotlib and plotly
+# Apply dark-themed fig 
+# make 1 options 
+# - 1) normal white theme
+# - 2) dark theme
+# as a function (probably independent + make different module
+#  for a visualization functions)
+
+# Lastly polish the matplotlib + plotly for really clear chart
 
 
+# -Maybe- Convert show_statistics (only for user to see) into a dataframe
+# and make it more readable, just better.
 
+# Edit trade function
 
+# Add some more complicated statistics with Numpy + some more necessary visualizations
+# ( Less is more )
 
-# later for statistics
-#Trades per day/week/month
-#Expectancy
-#Profit Factor (gdy dodasz realized R)
-#Sharpe Ratio (kiedyś)
+# Custom chart colors??? (still not sure about that)
+
+#...
+
